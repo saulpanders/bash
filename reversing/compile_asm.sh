01054b45ad8e37e882477f4e3ca89bc31a1cf45e
Saul Panders <phs3mr@virginia.edu>
2021-10-08 21:26:23 -0400

    adding some utils for binary investigation

---
+++ reversing/compile_asm.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+"#compile assembly to raw (-f) machine code"
+nasm boot sect.asm -f bin -o boot sect.bin
---
+++ reversing/dump_bin.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+#Display binary/raw machine code as readable HEX
+od -t x1 -A n boot sect.bin
